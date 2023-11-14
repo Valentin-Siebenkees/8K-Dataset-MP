3c335df9b36e05b9cf4f7fe3caa7b04602fdc2c6
Valentin Siebenkees <65539354+Valentin-Siebenkees@users.noreply.github.com>
2023-11-14 20:38:52 +0100

    Update downscale_videos.py
    added downscaling_algorithm

--- downscale_videos.py
+++ downscale_videos.py
@@ -5,6 +5,18 @@ from os.path import exists
 base_path = 'E:/Medienprojekt_8K_Datensatz/Export'
 output_path = 'E:/Medienprojekt_8K_Datensatz/Output'
 
+# Downscaling algorithm exampels (For a list of available options see -sws_flags under -> ffmpeg -h filter=scale):
+#    - fast_bilinear
+#    - bilinear
+#    - bicubic (Default)
+#    - neighbour
+#    - area
+#    - lanczos
+#    - sinc
+#    - gauss
+#    - spline
+downscaling_algorithm = 'neighbour'
+
 if not exists(output_path):
     makedirs(output_path)
 
@@ -12,5 +24,5 @@ output_width = 3840
 output_height = 2160
 
 for clip in listdir(base_path):
-    cmd = f'ffmpeg -i {base_path}/{clip} -vf scale="{output_width}:{output_height}" {output_path}/downscaled_{clip}'
+    cmd = f'ffmpeg -i {base_path}/{clip} -vf scale="{output_width}x{output_height}:flags={downscaling_algorithm}" {output_path}/downscaled_{clip}'
     subprocess.run(cmd, shell=True)
