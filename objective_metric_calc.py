cc958a3f39cfdd747fdfa46a2e168f4cd7e6957e
Valentin Siebenkees <65539354+Valentin-Siebenkees@users.noreply.github.com>
2023-11-14 20:48:15 +0100

    Update objective_metric_calc.py
    added scaling_algorithm

--- objective_metric_calc.py
+++ objective_metric_calc.py
@@ -6,6 +6,21 @@ reference_path = 'E:/Medienprojekt_8K_Datensatz/Export'
 downscaled_path = 'E:/Medienprojekt_8K_Datensatz/Output'
 result_path = 'E:/Medienprojekt_8K_Datensatz/Objective_Metric_Results'
 
+# Scaling algorithm examples:
+#    - fast_bilinear
+#    - bilinear
+#    - bicubic (Default)
+#    - bicublin
+#    - neighbour
+#    - area
+#    - lanczos
+#    - sinc
+#    - gauss
+#    - spline
+#    - experimental
+
+scaling_algorithm = 'neighbour'
+
 if not exists(result_path):
     makedirs(result_path)
 
@@ -13,7 +28,7 @@ reference_clips = listdir(reference_path)
 downscaled_clips = listdir(downscaled_path)
 
 for i in range(len(reference_clips)):
-    ffqm = FfmpegQualityMetrics(f'{reference_path}/{reference_clips[i]}', f'{downscaled_path}/{downscaled_clips[i]}')
+    ffqm = FfmpegQualityMetrics(f'{reference_path}/{reference_clips[i]}', f'{downscaled_path}/{downscaled_clips[i]}', scaling_algorithm=scaling_algorithm)
     metrics = ffqm.calculate(["ssim", "psnr"])
     json_dump = ffqm.get_results_json()
     with open(f'{result_path}/{reference_clips[i]}_res.json', 'w') as f:
